fc17dbea437a91d9cf03234d104482fa15fe51b4
Sam Bisbee <sam@sbisbee.com>
2011-01-20 18:13:07 -0500

    Getting tags for a repo and validating them server side.

--- webRoot/create.php
+++ webRoot/create.php
@@ -8,8 +8,17 @@ require_once('./inc/autoloader.php');
 
 if($_POST['doCreate'])
 {
-  if(!$_POST['username'] || !$_POST['password'] || !$_POST['repo'])
-    throw new Exception('invalid input');
+  if(!$_POST['username'])
+    throw new Exception('Forgot username.');
+
+  if(!$_POST['password'])
+    throw new Exception('Forgot password.');
+
+  if(!$_POST['repo'])
+    throw new Exception('Forgot repo.');
+
+  if(!is_array($_POST['hash']) || count($_POST['hash']) <= 0)
+    throw new Exception('No tags selected.');
 
   $github = new phpGitHubApi();
 
@@ -31,7 +40,17 @@ if($_POST['doCreate'])
       if($repoInfo['fork'] || $repoInfo['parent'])
         throw new Exception('You cannot add a forked repository.');
 
-      echo "all's well"; 
+      $availableTags = $github->getRepoApi()->getRepoTags($_POST['username'], $_POST['repo']);
+
+      foreach($_POST['hash'] as $hash)
+      {
+        //TODO error if a submitted tag is already added
+
+        if(!in_array($hash, $availableTags))
+          throw new Exception('That is not a tag in your repository.');
+
+        //TODO get plugin.json from each tag here
+      } 
     }
     catch(phpGitHubApiRequestException $e)
     {
@@ -52,20 +71,45 @@ if($_POST['doCreate'])
 
       throw new Exception($msg);
     }
-    catch(Exception $e)
-    {
-      throw new Exception('there was a problem');
-    }
   }
 }
 ?>
 
 <html>
+  <head>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+      var getTagsButton = $('#getTags');
+
+      $(getTagsButton).click(function() {
+        var user = $(getTagsButton).parent().children('input[name=username]').val();
+        var repo = $(getTagsButton).parent().children('input[name=repo]').val();
+
+        $.ajax({
+          url: 'http://github.com/api/v2/json/repos/show/'+user+'/'+repo+'/tags',
+          dataType: 'jsonp',
+          success: function(data) {
+            for(var name in data.tags)
+              $('#tags').append('<li><input type="checkbox" name="hash[]" value="'+data.tags[name]+'"/> '+name);
+          }
+        });
+
+        return false;
+      });
+    });
+    </script>
+  </head>
   <body>
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
       <input type="hidden" name="doCreate" value="1"/>
-      Repo <input type="text" name="repo" value="sag"/><br/>
       User <input type="text" name="username" value="sbisbee"/><br/>
+      Repo <input type="text" name="repo" value="sag"/><br/>
+
+      <a href="#" id="getTags">Get Tags</a>
+      <ul id="tags"></ul>
+
+      JSON file location <input type="text" name="jsonPath"/>/plugin.json<br/>
       Pass <input type="password" name="password"/><br/>
       <input type="submit">
     </form>
